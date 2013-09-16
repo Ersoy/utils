2cfa6471af102bc1cdcd69687a44d7e6a72c8a2e
unknown <Ersoy.Hasanoglu@SKL-ERSOJ-HASAN.seavus.biz>
2013-09-16 10:21:54 +0200

    Added UriBuilderExtensions.

---
+++ Utils/src/Common/UriBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+
+namespace Utils.Common {
+
+    public static class UriBuilderExtensions {
+
+        public static void SetQueryStringParameter(this UriBuilder source, string key, object value) {
+            if (String.IsNullOrWhiteSpace(key)) {
+                throw new ArgumentNullException("key");
+            }
+            if (value == null) {
+                throw new ArgumentNullException("value");
+            }
+
+            var converter = TypeDescriptor.GetConverter(value.GetType());
+            var coll = source.Uri.ParseQueryString();
+            coll[key] = converter.ConvertToString(value);
+
+            const string KEY_VALUE_CONCATENATOR = "=";
+            const string QUERY_CONCATENATOR = "&";
+
+            var result = coll.AllKeys.Select(k => String.Concat(k, KEY_VALUE_CONCATENATOR, coll[k]));
+            source.Query = String.Join(QUERY_CONCATENATOR, result);
+        }
+    }
+
+}
---
+++ Utils/test/Common.Test/UriBuilderExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Utils.Common;
+
+namespace Common.Test {
+
+    public class UriBuilderExtensionsTest {
+
+        [Test]
+        public void AddsNewParameter() {
+            // Arrange
+            const string URL = "http://root.com?q=root";
+            var ub = new UriBuilder(URL);
+
+            // Act
+            ub.SetQueryStringParameter("key", "value");
+
+            // Assert
+            Assert.AreEqual("http://root.com/?q=root&key=value", ub.Uri.ToString());
+        }
+
+        [Test]
+        public void OverridesExistingParameter() {
+            // Arrange
+            const string URL = "http://root.com/?key=val";
+            var ub = new UriBuilder(URL);
+
+            // Act
+            ub.SetQueryStringParameter("key", "value");
+
+            // Assert
+            Assert.AreEqual("http://root.com/?key=value", ub.Uri.ToString());
+        }
+    }
+
+}
